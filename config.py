95f3b0d797b14ab629d517bffd8eea45e843dfe9
r78389357 <r78389357@cdzhongguan.com>
2026-04-09 18:06:48 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    """
+    全局配置
+    """
+    NETWORK_NAME = "PyChain-Mainnet"
+    BLOCK_TIME = 10 # 秒
+    MAX_BLOCK_SIZE = 1024 * 1024 # 1MB
+    RPC_PORT = 8545
+    P2P_PORT = 30303
